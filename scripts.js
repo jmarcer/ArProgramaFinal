b752393ce17387ad5ba0fd7cd420b74568ca5052
Julieta <marcerjulieta@gmail.com>
2023-04-06 00:49:42 -0300

    funcion para llenar form no editable, se armo la estructura, falta modificar para cada caso

---
+++ scripts.js
@@ -0,0 +1,5 @@
+function llenarPantallaConfirmacion(){
+    console.log("llenarPantallaConfirmacion");
+    document.getElementById("valorNombreApellido").textContent = document.getElementById("datoNombreApellido").value;
+
+}
